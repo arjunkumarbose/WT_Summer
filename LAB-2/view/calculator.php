4e4a59cf26d9ac583d685d6ae691dd9c60092ecc
Arjun Kumar Bose <82363264+arjunkumarbose@users.noreply.github.com>
2022-05-30 06:50:04 +0600

    Create calculator.php

---
+++ LAB-2/view/calculator.php
@@ -0,0 +1,19 @@
+<html>
+    <body>
+        <form action="../control/process.php" method="post">
+            <center>
+                    Number 1:
+                    <input type="text" name="no1" placeholder="no1">
+                    <br>
+                    Number 2:
+                    <input type="text" name="no2" placeholder="no1">
+                    <br>
+                    <input type="radio" name="button" value="+">+
+                    <input type="radio" name="button" value="-">-
+                    <input type="radio" name="button" value="*">*
+                    <input type="radio" name="button" value="/">/
+                    <br>
+                    <input type="submit" value="Result">                       
+        </form>
+    </body>
+</html>
