e6ff968ecd689dac01aebcf61a816e9c045d3783
Arjun Kumar Bose <82363264+arjunkumarbose@users.noreply.github.com>
2022-05-30 06:47:33 +0600

    Create mypage.php

---
+++ LAB-1/mypage.php
@@ -0,0 +1,77 @@
+<html>
+    <body>
+        <center>
+        <h1 align="center">Welcome! My Page</h1>
+        </center>
+        <center>
+        Website Url:
+        <a href="https://portal.aiub.edu/">AIUB</a>
+        </center>
+        <center>
+        <img src="aiub-campus.jpeg" height="400" width="300">
+        </center>
+    <form>
+        <center>
+        <br>
+        <table>
+        <tr><td>
+        First Name:
+        </td><td>
+        <input type="text" name="fname" placeholder="firstname">
+        </td></tr>
+
+        <tr><td>
+        Gender:
+        </td><td>
+        <input type="radio" name="gender" value="male">Male
+        <input type="radio" name="gender" value="female">Female
+        </td></tr>
+        
+        <tr><td>
+        Enter a Password:
+        </td><td>
+        <input type="password" name="pass" placeholder="password">
+        </td></tr>
+        
+        <tr><td>
+        Major in:
+        </td><td>
+        <input type="checkbox" name="major1" value="depatment1">Software Engineering
+        </td> <td>
+        <input type="checkbox" name="major2" value="depatment2">Matchine Learning
+        </td></tr>
+        
+        <tr><td>
+        Date of Birth:
+        </td><td>
+        <input type="date" name="dob" value="dateofbirth">
+        </td></tr>
+        
+        <tr><td>
+        Enter Email:
+        </td><td>
+        <input type="email" name="mail" value="mailaddress">
+        </td></tr>
+
+        <tr><td>
+        Enter Mobile Number:
+        </td><td>
+        <input type="number" name="mobileno" value="mobileNumber">
+        </td></tr>
+
+        <tr><td>
+        Enter your current time:
+        </td><td>
+        <input type="time" name="time" value="timezone">
+        </td></tr>
+        
+        <tr><td>
+        <input type="submit" name="submit" value="Register">
+        </td><td>
+        <input type="reset" name="reset" value="Reset All">
+        </td></tr>
+    </table>>
+    </form>
+    </center>
+    </body>
+</html>
